e74710af2fbe278f967d746b57c094b03f2d76eb
Joshua Hans <jhans@schools.nyc.gov>
2020-10-13 08:47:16 -0400

    add readibility

--- Shapes.java
+++ Shapes.java
@@ -3,10 +3,12 @@ import java.util.*;
 public class Shapes{
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
+
     System.out.print("Enter the side length: ");
     double side = input.nextDouble();
+
     square(side);
-  }
+  }//end main method
 
   /*
    * N: square
@@ -15,11 +17,11 @@ public class Shapes{
    * R:
    */
   public static void square(double s){
-    //side times side
+    //area = side times side
     double a = s * s;
 
     //generate some output
     System.out.println("A square with side " + s + " has an area of " + a);
   }//close square method
 
-}
+}//end class
